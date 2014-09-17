4d5f0951c35cd3da10c2b5708db4d12ef1ee7ca3
amaiafdez <afernandezvi13dw@ikzubirimanteo.com>
2014-09-17 12:18:17 +0200

    validado del todo

--- tabla/tabla.php
+++ tabla/tabla.php
@@ -4,8 +4,9 @@
     <table>
 		<?php 
 			$num = "";
-			if(empty($_GET["numero"])){ //isset -> dice si está lleno
+			if(empty($_GET["numero"])||$_GET["numero"]<0){ //isset -> dice si está lleno
 				$num =2;				//empty -> dice si está vacío
+				echo "Hay que introducir un número mayor que 0";
 			}else{
 				
 				$num = $_GET["numero"];
