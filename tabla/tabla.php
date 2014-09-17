1b4491c979619baacc83384c6aa77f2b1c42525e
amaiafdez <afernandezvi13dw@ikzubirimanteo.com>
2014-09-17 12:15:33 +0200

    validado

--- tabla/tabla.php
+++ tabla/tabla.php
@@ -3,8 +3,20 @@
 	
     <table>
 		<?php 
-			$num = $_GET["numero"];
-	    	
+			$num = "";
+			if(empty($_GET["numero"])){ //isset -> dice si está lleno
+				$num =2;				//empty -> dice si está vacío
+			}else{
+				
+				$num = $_GET["numero"];
+				
+			}
+			if(is_numeric($_GET["numero"])){
+				$num = $_GET["numero"];
+			}else{
+				echo "Hay que introducir un número";
+			}
+						    	
 	    	for ($i=0; $i < $num; $i++) { 
 	    	echo'<tr>
 	    			<td>hh</td>
