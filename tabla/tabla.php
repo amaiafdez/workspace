9dd25cf417e67c310371ef56e830ff460da58f95
amaiafdez <afernandezvi13dw@ikzubirimanteo.com>
2014-09-17 11:49:45 +0200

    aa

---
+++ tabla/tabla.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+	
+    <table>
+		<?php 
+			$num = $_GET["numero"];
+	    	
+	    	for ($i=0; $i < $num; $i++) { 
+	    	echo'<tr>
+	    			<td>hh</td>
+	    		</tr>';
+	    	}
+     	?>
+     </table>
+</body>
+</html>
